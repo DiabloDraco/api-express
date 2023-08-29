f2c5f9373f19c051032f57db4bd8c412033d7099
DiabloDraco~ <mirmuhsinmirahmatov@gmail.com>
2023-08-29 15:44:26 +0500

    add

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import videoRouter from './routes/video.route.js';
 const PORT = process.env.PORT || 3000
 const app = express();
 
-const corsOptions = {
-    origin: 'http://expert.uz',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-app.use(cors(corsOptions))
+// const corsOptions = {
+//     origin: 'http://expert.uz',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+app.use(cors())
 app.use(express.json())
 app.use(videoRouter)
 
