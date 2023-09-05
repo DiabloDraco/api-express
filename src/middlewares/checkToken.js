fb0c324e35b53fb1355817bfebd9c6454e58cbb2
DiabloDraco <mirmuhsinmirahmatov@email.com>
2023-09-05 18:57:42 +0500

    add

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -3,7 +3,7 @@ export default async (req, res, next) => {
         await fetch('https://api.expert.uz/api/public/check/token?user_token=' + req.query.token, {
             method: "GET"
         }).then(res => res.json()).then(data => {
-            if (data.data.check.case == true) {
+            if (data.data.check == true) {
                 return next()
             } else {
                 res.status(401).json({ status: 401, message: 'Unauthorized user' })
