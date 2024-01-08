fa9267847f9edab8d171ff811e31897aadfce50f
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2024-01-08 17:09:23 +0500

    add

--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -1,24 +1,31 @@
 import { Router } from "express";
-import { DELETEVIDEO, GETVIDEO, GETVIDEONAMES, POSTVIDEO } from "../controllers/video.controller.js";
-import multer from 'multer';
+import {
+  DELETEVIDEO,
+  GETVIDEO,
+  GETVIDEONAMES,
+  POSTVIDEO,
+  TEST,
+} from "../controllers/video.controller.js";
+import multer from "multer";
 import checkToken from "../middlewares/checkToken.js";
 
-
 const upload = multer({
-    dest: 'videos/', limits: {
-        fileSize: 1073741824
-    }
+  dest: "videos/",
+  limits: {
+    fileSize: 1073741824,
+  },
 });
 
+const videoRouter = Router();
 
-const videoRouter = Router()
+videoRouter.get("/videos/getNames", checkToken, GETVIDEONAMES);
 
-videoRouter.get('/videos/getNames', checkToken, GETVIDEONAMES)
+videoRouter.post("/videos", checkToken, upload.single("video"), POSTVIDEO);
 
-videoRouter.post('/videos', checkToken, upload.single('video'), POSTVIDEO);
+videoRouter.get("/videos", checkToken, GETVIDEO);
 
-videoRouter.get('/videos', checkToken, GETVIDEO);
+videoRouter.delete("/videos/:name", checkToken, DELETEVIDEO);
 
-videoRouter.delete('/videos/:name', checkToken, DELETEVIDEO)
+videoRouter.get("/test", TEST);
 
-export default videoRouter
+export default videoRouter;
